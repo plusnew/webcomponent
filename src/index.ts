7d09464a687d06f30fcdd23c62aff4212082b0a9
plusgut <github@webentwickler2-0.de>
2023-10-21 14:18:06 +0200

    feat(parent): implement searching for parents

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,15 @@ declare global {
       } & {
         children?: ShadowElement;
       };
+
+      slot: {
+        [K in keyof HTMLSlotElement as Exclude<
+          K,
+          "children"
+        >]?: HTMLSlotElement[K];
+      } & {
+        children?: ShadowElement;
+      };
     }
   }
 }
@@ -47,28 +56,62 @@ export function mount(parent: HTMLElement, JSXElement: ShadowElement) {
   return shadowResult.node;
 }
 
-type PartialHtmlElement = Partial<HTMLElement>;
+type PartialHtmlElement = {
+  [K in keyof HTMLElement as Exclude<K, "children">]?: HTMLElement[K];
+};
 
 export function webcomponent<T extends { render: () => ShadowElement }>(
   name: string,
   Webcomponent: Webcomponent<T>,
 ): (
-  properties: {
+  properties: PartialHtmlElement & {
     [K in keyof T as Exclude<K, "render" | keyof WebComponent>]: T[K];
-  } & PartialHtmlElement,
+  },
 ) => null {
   customElements.define(name, Webcomponent as any);
 
   return name as any;
 }
 
-export abstract class WebComponent extends (HTMLElement as any as null) {
+export abstract class WebComponent extends HTMLElement {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  findParent<T>(needle: { new (): T }): T | null {
+    throw new Error("Element is not mounted and cant't find parents yet");
+  }
   connectedCallback(this: HTMLElement & WebComponent) {
     const shadowRoot = this.attachShadow({ mode: "open" });
     const shadowCache: ShadowCache = {
       node: null,
       nestedShadows: [],
       value: false,
+    };
+    const parentsCache = new Map();
+
+    const findParent = function <T>(
+      haystack: ParentNode | null,
+      needle: { new (): T },
+    ): T | null {
+      return haystack === null
+        ? null
+        : haystack instanceof needle
+        ? haystack
+        : haystack instanceof WebComponent
+        ? haystack.findParent(needle)
+        : findParent(haystack.parentNode, needle);
+    };
+
+    this.findParent = (needle) => {
+      if (parentsCache.has(needle) === false) {
+        parentsCache.set(needle, findParent(this.parentNode, needle));
+      }
+      return parentsCache.get(needle);
+    };
+
+    (this as any).disconnectedCallback = () => {
+      disconnect();
+      parentsCache.clear();
+
+      // @TODO remove event-listeners of nestedShadowElements
     };
 
     const disconnect = effect(() => {
@@ -77,12 +120,6 @@ export abstract class WebComponent extends (HTMLElement as any as null) {
         reconcile(shadowRoot, null, shadowCache, result);
       });
     });
-
-    (this as any).disconnectedCallback = () => {
-      disconnect();
-
-      // @TODO remove event-listeners of nestedShadowElements
-    };
   }
   abstract render(): ShadowElement;
 }
--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,5 +1,6 @@
 import { expect } from "@esm-bundle/chai";
 import { mount, prop, webcomponent, WebComponent } from "@plusnew/webcomponent";
+import { jsx } from "@plusnew/webcomponent/jsx-runtime";
 import { signal } from "@preact/signals-core";
 
 describe("webcomponent", () => {
@@ -139,5 +140,51 @@ describe("webcomponent", () => {
     (component as any).show = false;
 
     expect(component.shadowRoot?.childNodes.length).to.equal(0);
+  });
+
+  it("finds context", () => {
+    class ProviderClass extends HTMLElement {
+      foo = signal("bar");
+      connectedCallback(this: HTMLElement & WebComponent) {
+        const shadowRoot = this.attachShadow({ mode: "open" });
+
+        shadowRoot.appendChild(document.createElement("slot"));
+      }
+    }
+    customElements.define("test-provider", ProviderClass);
+
+    const Component = webcomponent(
+      "test-consumer",
+      class Component extends WebComponent {
+        render() {
+          const providerElement = this.findParent(ProviderClass);
+          return providerElement?.foo.value ?? false;
+        }
+      },
+    );
+
+    mount(container, jsx("test-provider", { children: [<Component />] }, null));
+
+    expect(container.childNodes.length).to.equal(1);
+
+    const providerElement = container.childNodes[0] as InstanceType<
+      typeof ProviderClass
+    >;
+
+    expect(providerElement.tagName).to.equal("TEST-PROVIDER");
+
+    const component = (
+      providerElement.shadowRoot?.childNodes[0] as HTMLSlotElement
+    ).assignedNodes()[0] as HTMLElement;
+
+    expect(component.tagName).to.equal("TEST-CONSUMER");
+    expect(component.shadowRoot?.childNodes.length).to.equal(1);
+    expect(component.shadowRoot?.textContent).to.equal("bar");
+
+    providerElement.foo.value = "baz";
+
+    expect(component.tagName).to.equal("TEST-CONSUMER");
+    expect(component.shadowRoot?.childNodes.length).to.equal(1);
+    expect(component.shadowRoot?.textContent).to.equal("baz");
   });
 });
