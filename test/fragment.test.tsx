e56d1ad7f8cee4348c99bf46c83efb0a69cd67a1
plusgut <github@webentwickler2-0.de>
2025-11-05 11:41:01 +0100

    feat(style): add style-handling to dom-elements

--- test/fragment.test.tsx
+++ test/fragment.test.tsx
@@ -3,48 +3,56 @@ import { createComponent, mount, prop } from "@plusnew/webcomponent";
 import { signal } from "@preact/signals-core";
 
 describe("fragment", () => {
-    let container: HTMLElement;
-  
-    beforeEach(() => {
-      container = document.createElement("div");
-      document.body.appendChild(container);
-    });
-  
-    afterEach(() => {
-      container.remove();
-    });
-  
-    it("creates basic component with fragment", () => {
-      const baz = signal("foo")
-      const Component = createComponent(
-        "test-base",
-        class Component extends HTMLElement {
-          @prop() accessor foo: string;
+  let container: HTMLElement;
 
-  
-          render() {
-            return <><div>{baz.value}</div><span /></>
-          }
-        },
-      );
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
 
-      mount(container, <Component foo="mep" className="some-class" />);
+  afterEach(() => {
+    container.remove();
+  });
 
-      expect(container.childNodes.length).to.equal(1);
+  it("creates basic component with fragment", () => {
+    const baz = signal("foo");
+    const Component = createComponent(
+      "test-base",
+      class Component extends HTMLElement {
+        @prop() accessor foo: string;
 
-      const component = container.childNodes[0] as HTMLElement;
-      const buttonElement = component.shadowRoot?.childNodes[0] as HTMLElement
-      expect(component.tagName).to.equal("TEST-BASE");
-      expect(component.className).to.equal("some-class");
-      expect(component.childNodes.length).to.equal(0);
-      expect(component.shadowRoot?.childNodes.length).to.equal(2);
-      expect((buttonElement).tagName).to.equal("DIV");
-      expect((buttonElement).textContent).to.equal("foo");
-      expect((component.shadowRoot?.childNodes[1] as HTMLElement).tagName).to.equal("SPAN");
+        render() {
+          return (
+            <>
+              <div>{baz.value}</div>
+              <span />
+            </>
+          );
+        }
+      },
+    );
 
-      baz.value = "bar";
+    mount(container, <Component foo="mep" className="some-class" />);
 
-      expect(component.shadowRoot?.childNodes[0] === buttonElement).to.equal(true);
-      expect((buttonElement).textContent).to.equal("bar");
-    })
-})
+    expect(container.childNodes.length).to.equal(1);
+
+    const component = container.childNodes[0] as HTMLElement;
+    const buttonElement = component.shadowRoot?.childNodes[0] as HTMLElement;
+    expect(component.tagName).to.equal("TEST-BASE");
+    expect(component.className).to.equal("some-class");
+    expect(component.childNodes.length).to.equal(0);
+    expect(component.shadowRoot?.childNodes.length).to.equal(2);
+    expect(buttonElement.tagName).to.equal("DIV");
+    expect(buttonElement.textContent).to.equal("foo");
+    expect(
+      (component.shadowRoot?.childNodes[1] as HTMLElement).tagName,
+    ).to.equal("SPAN");
+
+    baz.value = "bar";
+
+    expect(component.shadowRoot?.childNodes[0] === buttonElement).to.equal(
+      true,
+    );
+    expect(buttonElement.textContent).to.equal("bar");
+  });
+});
